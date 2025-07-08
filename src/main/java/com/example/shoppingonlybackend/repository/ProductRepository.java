e1ffb34a76a82952dddb491ef818a416d073c83b
altugucar151 <altug.ucar@ug.bilkent.edu.tr>
2025-07-08 16:10:41 +0300

    Update ProductRepository.java

--- src/main/java/com/example/shoppingonlybackend/repository/ProductRepository.java
+++ src/main/java/com/example/shoppingonlybackend/repository/ProductRepository.java
@@ -2,7 +2,15 @@ package com.example.shoppingonlybackend.repository;
 
 import com.example.shoppingonlybackend.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProductRepository  extends JpaRepository<Product, Integer> {
+       List<Product> findByNameContaining(String keyword);
 
+    List<Product> findByPriceGreaterThan(double price);
+
+    @Query("SELECT p FROM Product p WHERE p.description LIKE %:keyword%")
+    List<Product> searchByDescriptionByDescription(String keyword);
 }
