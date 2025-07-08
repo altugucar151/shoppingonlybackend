4726c119c05dbe5ef3e4c2efcad7a267d4409cfd
m. okan <mokan.yilmaz@sikayetvar.dev>
2025-07-08 15:03:21 +0300

    code review yapıldı

---
+++ src/main/java/com/example/shoppingonlybackend/payload/LoginDTO.java
@@ -0,0 +1,15 @@
+package com.example.shoppingonlybackend.payload;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class LoginDTO {
+    String token;
+}
